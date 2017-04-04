ec527713ea35de4970e7ee36c6347df7d79672de
Floyda <floyda@163.com>
2017-04-04 23:23:46 +0800

    [Fixed] You must use sublime.load_resource to read file contents of a file within a package if it is installed as a .sublime-syntax archive. This does not feature a directory listing, though. You should instead look to store the doc info files as a single JSON file instead of using the file system for this, and then load that with sublime.load_resource into json.loads.

--- wxapp.py
+++ wxapp.py
@@ -2,47 +2,24 @@ import sublime
 import sublime_plugin
 import os
 import copy
+import json
 
 
 # ----------------------------------------------------------
 # Config
 # ----------------------------------------------------------
-def get_st_version():
-    return 2 if sys.version_info < (3,) else 3
+def get_api_url():
+    return 'https://mp.weixin.qq.com/debug/wxadoc/dev/api/'
 
-_join = os.path.join
-def get_wxapp_path():
-    return _join(sublime.packages_path(), 'wxapp')
 
-def get_doc_path():
-    return _join(get_wxapp_path(), 'doc')
-
-def get_api_path():
-    return _join(get_doc_path(), 'api')
-
-def get_framework_path():
-    return _join(get_doc_path(), 'framework')
+def get_api_json_path():
+    return "/".join(["Packages", "wxapp", "wxapp_api.json"])
 
 
 # ----------------------------------------------------------
 # Controller
 # ----------------------------------------------------------
-def make_doc_list():
-    return os.listdir(get_api_path())
-
-def parse_doc_file(path):
-    with open(path, 'r', encoding='utf8') as fp:
-        content = fp.readlines()
-        url = content[0]
-        desc = content[1]
-        info = {
-            'url': url,
-            'desc': desc
-        }
-        return info
-
 def open_url(url):
-    command = ''
     if sublime.platform() == 'osx':
         command = 'open'
     elif sublime.platform() == 'windows':
@@ -52,30 +29,33 @@ def open_url(url):
     command += ' ' + str(url)
     os.system(command)
 
+
 def open_api_url(url):
-    prefix = 'https://mp.weixin.qq.com/debug/wxadoc/dev/api/'
+    prefix = get_api_url()
     url = prefix + url
     open_url(url)
+
 
 # ----------------------------------------------------------
 # Commands
 # ----------------------------------------------------------
-class OpenDocumentCommand(sublime_plugin.WindowCommand):
+class OpenApiDocument(sublime_plugin.WindowCommand):
     def run(self):
-        doc_list = make_doc_list()
-        doc_bak = copy.copy(doc_list)
+        api_dict = json.loads(sublime.load_resource(get_api_json_path()))
+        api_list = list(api_dict.keys())
+        api_list = sorted(api_list)
+        show_list = copy.copy(api_list)
 
-        for i in range(len(doc_bak)):
-            prefix = get_api_path()
-            path = _join(prefix, doc_bak[i])
-            info = parse_doc_file(path)
-            doc_bak[i] = 'wx.%s  (%s)' % (doc_bak[i], info['desc'])
+        for i in range(len(show_list)):
+            api = api_list[i]
+            info = api_dict[api]
+            show_list[i] = '%s  (%s)' % (api, info['desc'])
 
         def on_done(index):
             if index == -1: return
-            prefix = get_api_path()
-            path = _join(prefix, doc_list[index])
-            info = parse_doc_file(path)
-            open_api_url(info['url'])
 
-        self.window.show_quick_panel(doc_bak, on_done)
+            api = api_list[index]
+            info = api_dict[api]
+            open_api_url(info['href'])
+
+        self.window.show_quick_panel(show_list, on_done)
