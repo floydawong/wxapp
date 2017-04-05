ffae1c0a96a80b9dc286fa284a9087729583df76
Floyda <floyda@163.com>
2017-04-05 14:03:03 +0800

    # Open Component Documentation

--- wxapp.py
+++ wxapp.py
@@ -9,15 +9,23 @@ import json
 # Config
 # ----------------------------------------------------------
 def get_api_url():
-    return 'https://mp.weixin.qq.com/debug/wxadoc/dev/api/'
+    return r'https://mp.weixin.qq.com/debug/wxadoc/dev/api/'
+
+
+def get_component_url():
+    return r'https://mp.weixin.qq.com/debug/wxadoc/dev/component/'
 
 
 def get_api_json_path():
     return "/".join(["Packages", "wxapp", "wxapp_api.json"])
 
 
+def get_component_json_path():
+    return "/".join(["Packages", "wxapp", "wxapp_component.json"])
+
+
 # ----------------------------------------------------------
-# Controller
+# Utils
 # ----------------------------------------------------------
 def open_url(url):
     if sublime.platform() == 'osx':
@@ -30,10 +38,32 @@ def open_url(url):
     os.system(command)
 
 
-def open_api_url(url):
-    prefix = get_api_url()
-    url = prefix + url
-    open_url(url)
+# ----------------------------------------------------------
+# Manager
+# ----------------------------------------------------------
+class WxappManager():
+    def open_doc(self, json_path, prefix_url):
+        doc_dict = json.loads(sublime.load_resource(json_path))
+
+        doc_list = list(doc_dict.keys())
+        doc_list = sorted(doc_list)
+
+        show_list = copy.copy(doc_list)
+
+        for i in range(len(show_list)):
+            api = doc_list[i]
+            info = doc_dict[api]
+            show_list[i] = '%s  (%s)' % (api, info['desc'])
+
+        def on_done(index):
+            if index == -1: return
+            api = doc_list[index]
+            info = doc_dict[api]
+            url = prefix_url + info['href']
+            open_url(url)
+
+        window = sublime.active_window()
+        window.show_quick_panel(show_list, on_done)
 
 
 # ----------------------------------------------------------
@@ -41,21 +71,15 @@ def open_api_url(url):
 # ----------------------------------------------------------
 class OpenApiDocument(sublime_plugin.WindowCommand):
     def run(self):
-        api_dict = json.loads(sublime.load_resource(get_api_json_path()))
-        api_list = list(api_dict.keys())
-        api_list = sorted(api_list)
-        show_list = copy.copy(api_list)
+        json_path = get_api_json_path()
+        prefix_url = get_api_url()
+        wmanager = WxappManager()
+        wmanager.open_doc(json_path, prefix_url)
 
-        for i in range(len(show_list)):
-            api = api_list[i]
-            info = api_dict[api]
-            show_list[i] = '%s  (%s)' % (api, info['desc'])
 
-        def on_done(index):
-            if index == -1: return
-
-            api = api_list[index]
-            info = api_dict[api]
-            open_api_url(info['href'])
-
-        self.window.show_quick_panel(show_list, on_done)
+class OpenComponentDocument(sublime_plugin.WindowCommand):
+    def run(self):
+        json_path = get_component_json_path()
+        prefix_url = get_component_url()
+        wmanager = WxappManager()
+        wmanager.open_doc(json_path, prefix_url)
